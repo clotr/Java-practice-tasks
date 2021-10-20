77bfa28dc91a64d6b9c6c2b5a8c1de96f44e0827
clotr <mosonyi.istvan@gmail.com>
2021-10-20 10:46:01 +0200

    added comments

--- DrawPyramid.java
+++ DrawPyramid.java
@@ -7,11 +7,11 @@ public class DrawPyramid {
         int lineCount = input.nextInt();
         int i = 0;
         int numberOfX = 1;
-        do {
-            for (int j = i; lineCount - j > 0; j++) {
+        do {                                                   // ez a ciklus felel a sorléptetésért
+            for (int j = i; lineCount - j > 0; j++) {          // ez a ciklus felel soron belül a szóközök kiíratásáért
                 System.out.print(" ");
                 if (lineCount - j == 1) {
-                    for (int k = 0; k < numberOfX; k++) {
+                    for (int k = 0; k < numberOfX; k++) {      // ez a ciklus felel soron belül az x-ek kiíratásáért
                         System.out.print("x");
                     }
                 }
